f2212ee547a420af1b41acf475e97a9b7c6bb926
Shivani Bodara <shivanibodara1@gmail.com>
2025-02-27 15:33:05 -0500

    fixed a typo

--- src/venueSlice.js
+++ src/venueSlice.js
@@ -42,7 +42,8 @@ export const venueSlice = createSlice({
       const { payload: index } = action;
       if (state[index]) {
         if (state[index].name === " Auditorium Hall (Capacity:200)" && state[index].quantity >= 3) {
-          return;        }
+          return;        
+        }
         state[index].quantity++;
       }
     },
